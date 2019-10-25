0b44f5cf2affdee8d2c8df26ddfe0d4ca3663942
Melody Stern <melliemuse@gmail.com>
2019-10-25 16:43:16 -0500

    got everything working!

--- national-parks/src/scripts/DOM-base.js
+++ national-parks/src/scripts/DOM-base.js
@@ -1,36 +1,40 @@
 console.log('this will contain html')
+let HTMLtemplate = ``
+const pageContainer = document.querySelector('.page__container')
 
 APIcall()
     .then(parsedParks => {
         console.log(parsedParks)
-        let HTMLtemplate = ``
+        
         parsedParks.forEach((park) => {
             DarkSkyAPICall(park)
                 .then(parsedWeather => {
                     console.log(parsedWeather)
 
-                        if (park.visited === true) {
-                            HTMLtemplate += `<article class='visited'>`
-                        } else {
-                            HTMLtemplate += `<article class='not_visited'>`
-                        }
+                    if (park.visited === true) {
+                        HTMLtemplate += `<article class='visited'>`
+                    } else {
+                        HTMLtemplate += `<article class='not_visited'>`
+                    }
 
-                        HTMLtemplate += `
-    <h3>${park.name}</h3>
-    <p>${park.state}</p>
-    </article>
-    <p>Weather:</p>
-    <ul>
-    <li>Currently: ${parsedWeather.currently.summary}</li>
-    <li>Today: ${parsedWeather.hourly.summary}</li>
-    <li>Week: ${parsedWeather.daily.summary}</li>
-    </ul>
-    `
-                
+                    HTMLtemplate += `
+                    <h3>${park.name}</h3>
+                    <p>${park.state}</p>
+                    <p>Weather:</p>
+                    <ul>
+                    <li>Currently: ${parsedWeather.currently.summary}</li>
+                    <li>Today: ${parsedWeather.hourly.summary}</li>
+                    <li>Week: ${parsedWeather.daily.summary}</li>
+                    </ul>
+                    </article>
+                    `
+                    console.log(HTMLtemplate)
+                    
+                    
+                    pageContainer.innerHTML = HTMLtemplate
                 })
         })
-        const pageContainer = document.querySelector('.page__container')
-        pageContainer.innerHTML += HTMLtemplate
 
+        
     })
 
